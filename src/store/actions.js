af453e6f2d46bf73f4e6df6a2b5fdb082a8b13e8
Jared Zook <jaredzook@gmail.com>
2020-09-02 06:33:25 -0700

    adding pnw lat/long to axios request

--- src/store/actions.js
+++ src/store/actions.js
@@ -6,6 +6,8 @@ export default {
     commit("FETCHING_TREES");
 
     try {
+      // https://www.flickr.com/services/api/flickr.photos.search.html
+
       const result = await axios({
         method: "get",
         url: "https://api.flickr.com/services/rest",
@@ -13,11 +15,18 @@ export default {
           method: "flickr.photos.search",
           api_key: config.api_key,
           tags: tag,
-          extras: "url_n, owner_name, date_taken, views",
+          extras: "url_n, owner_name, date_taken, views, machine_tags",
           page: 1,
           format: "json",
           nojsoncallback: 1,
           per_page: 30,
+          safe_search: true,
+          content_type: 1, // photos only
+          geo_context: 2, // outdoors
+          // seattle-ish
+          lat: 47.000499,
+          long: -122.003108,
+          radius: 200, // miles
         },
       });
 
