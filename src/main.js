c0112c84ffac1e37e10c802f2f72377b8eb8c2fb
Jared Zook <jaredzook@gmail.com>
2020-09-02 05:40:17 -0700

    added vuex store

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
+import store from "./store";
 import { BootstrapVue } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
-Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
+Vue.use(BootstrapVue);
+
 new Vue({
+  store,
   render: (h) => h(App),
 }).$mount("#app");
