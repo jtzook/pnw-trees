3145de78ef5744b19ff4e4315e486a61b2eb4faa
Jared Zook <jaredzook@gmail.com>
2020-10-24 18:35:41 -0700

    leaves in a circle

--- src/components/LoadingAnimation.vue
+++ src/components/LoadingAnimation.vue
@@ -1,50 +1,42 @@
 <template>
-  <ul class="container">
-    <li v-for="(leaf, index) in 8" :key="index" class="leaf"></li>
-  </ul>
+  <div class="container">
+    <div v-for="(leaf, index) in 8" :key="index" class="leaf"></div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-@mixin tree-circle($item-count, $circle-size) {
+.container {
+  $numLeaves: 8;
+
+  height: 400px;
+  width: 400px;
+  border: 1px solid black;
   position: relative;
-  width: $circle-size;
-  height: $circle-size;
-  padding: 0;
 
-  $numTrees: 8;
-
-  li {
+  .leaf {
     display: block;
     position: absolute;
-    top: 50%;
-    left: 50%;
-    margin: -3em -1.5em;
-    height: 25%;
+    top: 8.45em;
+    left: 10.45em;
 
-    $angle: (360 / $numTrees);
+    $angle: (360 / $numLeaves);
     $rotationDeg: 0;
 
-    @for $i from 1 through $numTrees {
+    @for $i from 1 through $numLeaves {
       &:nth-of-type(#{$i}) {
         transform: rotate($rotationDeg * 1deg)
-          translate(9em)
-          rotate($rotationDeg * -1deg);
+          translate(8.5em)
+          rotate($rotationDeg * -1deg)
+          rotate(($angle * $i) * 1deg + 10deg)
+          rotate(405deg);
       }
 
       $rotationDeg: $rotationDeg + $angle;
     }
   }
-}
-
-.container {
-  @include tree-circle($item-count: 8, $circle-size: 20em);
-
-  padding: 20px;
-  height: 400px;
-  width: 400px;
-  border: 1px solid black;
 
   .leaf {
+    height: 8em;
     content: url("../../assets/black-leaf.png");
 
     &.active {
