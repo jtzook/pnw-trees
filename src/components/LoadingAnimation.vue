869ff5e6404c8d146d11c2d4eb8112292a5c016a
Jared Zook <jaredzook@gmail.com>
2020-10-25 11:41:36 -0700

    set compass directions

--- src/components/LoadingAnimation.vue
+++ src/components/LoadingAnimation.vue
@@ -1,46 +1,55 @@
 <template>
   <div class="container">
-    <div v-for="(leaf, index) in 8" :key="index" class="leaf"></div>
+    <div class="leaf-compass">
+      <div
+        v-for="direction in compassDirections"
+        :key="direction"
+        :class="['leaf', direction]"
+      ></div>
+    </div>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      compassDirections: ["n", "ne", "e", "se", "s", "sw", "w", "nw"]
+    };
+  }
+};
+</script>
+
 <style lang="scss" scoped>
 .container {
-  $numLeaves: 8;
-
-  height: 400px;
-  width: 400px;
+  height: 250px;
+  width: 250px;
   border: 1px solid black;
-  position: relative;
+}
+
+.leaf-compass {
+  $numLeaves: 8;
 
   .leaf {
     display: block;
     position: absolute;
-    top: 8.45em;
-    left: 10.45em;
+    top: 21em;
+    left: 13.5em;
+    height: 6em;
 
     $angle: (360 / $numLeaves);
     $rotationDeg: 0;
 
+    content: url("../../assets/black-leaf.png");
+
     @for $i from 1 through $numLeaves {
       &:nth-of-type(#{$i}) {
         transform: rotate($rotationDeg * 1deg)
-          translate(8em)
-          rotate($rotationDeg * -1deg)
-          rotate(($angle * $i) * 1deg + 11deg)
-          rotate(405deg);
+          translate(5em)
+          rotate($rotationDeg * -1deg);
       }
 
       $rotationDeg: $rotationDeg + $angle;
-    }
-  }
-
-  .leaf {
-    height: 8em;
-    content: url("../../assets/black-leaf.png");
-
-    &.active {
-      content: url("../../assets/green-leaf.png");
     }
   }
 }
